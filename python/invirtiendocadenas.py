ba07d2174031363810bb6b47f8afc2805e2269bc
Galahud Codename <Mi7uProgramming@gmail.com>
2024-03-29 10:46:07 -0600

    correcion-de-programa

--- python/invirtiendocadenas.py
+++ python/invirtiendocadenas.py
@@ -1,12 +1,12 @@
 cadenas=str(input("Dime una palabra a invertir: "))
 
-cantidad=int(0)
+cantidad=int(-1)
 
 invertido=[]
 
 for letras in cadenas:
     invertido.append(letras)
-    cantidad+=1
+    cantidad-=1
 
-for letras_invertidas in range(0, cantidad, -1):
+for letras_invertidas in range(-1, cantidad, -1):
     print(invertido[letras_invertidas]) 
