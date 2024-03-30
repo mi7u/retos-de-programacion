58732f6466ec097dc3586a28c4add8d9f8e18903
Galahud Codename <Mi7uProgramming@gmail.com>
2024-03-30 04:46:53 -0600

    bug-fixed-and- comments-added

--- js/eliminandoCaracteres/script.js
+++ js/eliminandoCaracteres/script.js
@@ -1,16 +1,23 @@
 function filtro() {
-    let str1 = document.getElementById("str1").value;
-    let str2 = document.getElementById("str2").value;
+    let str1 = document.getElementById("str1").value.toLowerCase();
+    let str2 = document.getElementById("str2").value.toLowerCase();
     let out1 = []
     let out2 = []
     
+    //Creo un for que recorre del 0 al limite de str1 
     for (let i = 0; i < str1.length; i++) {
+     
+        //creo una condicion que es "si str2 NO incluye la letra de str1 ubicada en (i)" al ser verdadera esa condicion se agrega esa letra a out1
         if (!str2.includes(str1[i])) {
             out1.push(str1[i]);
         };
     };
 
+     //Creo un for que recorre del 0 al limite de str2
+
     for (let i = 0; i < str2.length; i++) {
+
+        //creo una condicion que es "si str1 NO incluye la letra de str2 ubicada en (i)" al ser verdadera esa condicion se agrega esa letra a out2
         if (!str1.includes(str2[i])) {
             out2.push(str2[i]);
         };
