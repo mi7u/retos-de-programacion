41572b2353aeb4b024acb9a6181efa0c0f293c75
Galahud Codename <Mi7uProgramming@gmail.com>
2024-03-31 12:29:27 -0600

    isPalindrome-added-+-bugfixed

---
+++ js/isPalindrome/script.js
@@ -0,0 +1,24 @@
+function palindromeAnalyzer() {
+    let palindrome = document.getElementById("initialWord").value.toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "").replace(/\s/g, "");
+
+        let lastLetter = palindrome.length - 1;
+        let isPalindrome = true;
+
+    for (let i = 0; i < palindrome.length; i++){
+
+        if (palindrome[i] !== palindrome[lastLetter - i]) {
+            isPalindrome = false
+            break
+        }
+    }
+
+    document.getElementById("palindromeResult").textContent = (isPalindrome) ? "The word in fact is palindrome" : "This isnt a palindrome";
+
+}
+
+
+function showBox() {
+    document.getElementById("button").addEventListener("click", function() {
+        document.getElementById("resultBox").style.display = "block";
+    });
+}
